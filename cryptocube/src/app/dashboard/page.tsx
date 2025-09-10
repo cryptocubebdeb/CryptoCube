e886d3ba89a82b4fdad3f5fa9260130f2d136d41
aliazahr <aach@solyasystems.com>
2025-09-09 20:15:46 -0400

    Creation of some folders for the sctructure of the website. Minimum UI just to know what page it is

---
+++ cryptocube/src/app/dashboard/page.tsx
@@ -0,0 +1,12 @@
+import Link from "next/link"
+export default function Page() 
+{
+    return ( 
+    <>
+    <div className="h-screen flex flex-col justify-center items-center space-y-6">
+      <h1 className="text-2xl text-center font-mono ">This should be your dashboard</h1>
+      <h1 className="text-xl text-center font-mono ">But you came here illegally, wait until you can create an account to see something...</h1>
+    </div> 
+  </>
+    )
+}
