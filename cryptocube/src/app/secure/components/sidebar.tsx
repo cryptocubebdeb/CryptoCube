5537cbf5c97594bd65ec0d11675ad6840eba0e05
Bao Ngoc Duong <2359970@bdeb.qc.ca>
2025-09-17 13:32:34 -0400

    move the deconnexion to the bottom of the sidebar

--- cryptocube/src/app/secure/components/sidebar.tsx
+++ cryptocube/src/app/secure/components/sidebar.tsx
@@ -7,25 +7,33 @@ const Links = [
     { href:"/secure/account/details", icon: <User size={18} />, text: 'Détails'},
     { href:"/secure/account/watchlist", icon: <Star size={18} />, text: 'Watchlist'},
     { href:"/secure/account/notifications",icon: <Bell size={18} />,  text: 'Notifications'},
-    { href:"/secure/account/settings", icon: <Settings size={18} />, text: 'Paramètres'},
-    { href:"", icon: <LogOut size={18} />, text:'Déconnexion'}
+    { href:"/secure/account/settings", icon: <Settings size={18} />, text: 'Paramètres'}
 ];
 
 
 const Sidebar = () => {
     return (
-        <aside className='w-64 min-h-screen text-white p-6'>
-            <h1 className='text-xl font-bold mb-8'>Mon Compte</h1>
-            <ul className='flex flex-col gap-6'>
-                {Links.map((link) => (
-                    <li key={link.href}>
-                        <Link href={link.href} className='flex items-center gap-3 hover:text-yellow-400 transition-colors'>
-                            {link.icon}
-                            <span className='font-mono'>{link.text}</span>
-                        </Link>
-                    </li>
-                ))}
-            </ul>
+        <aside className='w-64 min-h-screen text-white p-6 flex flex-col justify-between'>
+            <div>
+                <h1 className='text-xl font-bold mb-8'>Mon Compte</h1>
+                <ul className='flex flex-col gap-6'>
+                    {Links.map((link) => (
+                        <li key={link.href}>
+                            <Link href={link.href} className='flex items-center gap-3 hover:text-yellow-400 transition-colors'>
+                                {link.icon}
+                                <span className='font-mono'>{link.text}</span>
+                            </Link>
+                        </li>
+                    ))}
+                </ul>
+            </div>
+
+            <div className='mb-20'>
+                <Link href="" className='flex items-center gap-3 hover:text-red-500 transition-colors'>
+                    <LogOut size={18} />
+                    <span className='font-mono'>Déconnexion</span>
+                </Link>
+            </div>
         </aside>
     );
 }
