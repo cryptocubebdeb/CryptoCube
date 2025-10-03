a837076a80d24b5319d527b3964ac44152413b35
aliazahr <aali86362@outlook.com>
2025-10-03 13:37:58 -0400

    sauvegarde

--- cryptocube/src/app/secure/specificCoin/[id]/page.tsx
+++ cryptocube/src/app/secure/specificCoin/[id]/page.tsx
@@ -15,24 +15,31 @@ export default async function Page({ params }: { params: { id: string } }) {
 
     const name = coinData?.name ?? id;
     const logo = coinData?.image?.large as string | undefined;
+    const symbole = coinData?.symbol ?? id;
 
     return (
         <>
             <div className={`h-screen flex flex-col ${geologica.className}`}>
                 <div className="flex flex-col flex-1 items-center">
                     <h1 className="w-full text-3xl font-mono mb-9 mt-12">{name}</h1>
-                    <div className="w-[90%]">
+                    <div className="w-[85%]">
                         <div className="flex justify-between items-center">
-                            {logo && (
-                                <Image
-                                    src={logo}
-                                    alt={`${name} logo`}
-                                    width={48}
-                                    height={48}
-                                    className="rounded"
-                                    priority
-                                />
-                            )}
+                            <div className="flex items-center gap-4 p-4">
+                                {logo && (
+                                    <Image
+                                        src={logo}
+                                        alt={`${name} logo`}
+                                        width={48}
+                                        height={48}
+                                        className="rounded"
+                                        priority
+                                    />
+                                )}
+                                <div className="flex items-baseline gap-2">
+                                    <h1 className="text-3xl">{name}</h1>
+                                    <span className="text-xl text-white/70">{symbole}</span>
+                                </div>
+                            </div>
                             <button className="bg-[#3B455E] hover:bg-[#15171E] text-white py-2 px-3 rounded">
                                 Modify in portfolio
                             </button>
