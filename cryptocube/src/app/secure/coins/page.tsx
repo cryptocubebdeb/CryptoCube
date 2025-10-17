007372bd59904b57f02e742ae94954b3ea40a7f9
Nadine <2344893@bdeb.qc.ca>
2025-10-17 14:20:57 -0400

    Changement icon coeur par etoile

--- cryptocube/src/app/secure/coins/page.tsx
+++ cryptocube/src/app/secure/coins/page.tsx
@@ -304,17 +304,17 @@ export default function Page() {
                                                         {userKey ? (
                                                             <button
                                                                 onClick={(e) => toggleWatch(e, coin.id)}
-                                                                className={`transition-colors p-1 rounded ${userWatchlist.has(coin.id) ? 'text-blue-500' : 'text-gray-400 hover:text-blue-400'}`}
+                                                                className={`transition-colors p-1 rounded ${userWatchlist.has(coin.id) ? 'text-yellow-500' : 'text-gray-400 hover:text-yellow-400'}`}
                                                                 aria-label={userWatchlist.has(coin.id) ? 'Remove from watchlist' : 'Add to watchlist'}
                                                             >
-                                                                {/* Heart SVG */}
+                                                                {/* Star SVG */}
                                                                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" className="w-4 h-4" strokeWidth={1.5}>
                                                                     <path 
                                                                         strokeLinecap="round" 
                                                                         strokeLinejoin="round" 
                                                                         stroke="currentColor"
                                                                         fill={userWatchlist.has(coin.id) ? "currentColor" : "none"}
-                                                                        d="M21 8.5c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 4 3 6.015 3 8.5c0 7.5 9 11.5 9 11.5s9-4 9-11.5z"
+                                                                        d="M11.48 3.499a.562.562 0 011.04 0l2.125 5.111a.563.563 0 00.475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 00-.182.557l1.285 5.385a.562.562 0 01-.84.61l-4.725-2.885a.563.563 0 00-.586 0L6.982 20.54a.562.562 0 01-.84-.61l1.285-5.386a.562.562 0 00-.182-.557l-4.204-3.602a.563.563 0 01.321-.988l5.518-.442a.563.563 0 00.475-.345L11.48 3.5z"
                                                                     />
                                                                 </svg>
                                                             </button>
