028b4be78e286a6d8aac848c9de9c89abaf061e0
aliazahr <aali86362@outlook.com>
2025-10-18 19:20:11 -0400

    remove turbopack

--- cryptocube/auth.config.ts
+++ cryptocube/auth.config.ts
@@ -3,17 +3,17 @@ import type { NextAuthConfig } from 'next-auth';
 export const authConfig = {
   providers: [], // always add providers even if empty
   pages: {
-    signIn: '/login',
+    signIn: '/auth/login',
   },
   callbacks: {
     authorized({ auth, request: { nextUrl } }) {
       const isLoggedIn = !!auth?.user;
-      const isOnDashboard = nextUrl.pathname.startsWith('/dashboard');
-      if (isOnDashboard) {
+      const isOnSecureDashboard = nextUrl.pathname.startsWith('/secure/dashboard');
+      if (isOnSecureDashboard) {
         if (isLoggedIn) return true;
         return false; // Redirect unauthenticated users to login page
       } else if (isLoggedIn) {
-        return Response.redirect(new URL('/dashboard', nextUrl));
+        return Response.redirect(new URL('/secure/dashboard', nextUrl));
       }
       return true;
     },
