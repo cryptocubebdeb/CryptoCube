dd12a1498ff8cae6df46533d70155ad6aadfc8a2
aliazahr <aali86362@outlook.com>
2025-10-20 07:01:39 -0400

    add correction to auth config to change url redirection

--- cryptocube/auth.config.ts
+++ cryptocube/auth.config.ts
@@ -1,21 +1,17 @@
-import type { NextAuthConfig } from 'next-auth';
- 
+// src/auth.config.ts
+import type { NextAuthConfig } from "next-auth";
+
 export const authConfig = {
-  providers: [], // always add providers even if empty
+  providers: [],
   pages: {
-    signIn: '/auth/login',
+    signIn: "/auth/login",
   },
   callbacks: {
-    authorized({ auth, request: { nextUrl } }) {
-      const isLoggedIn = !!auth?.user;
-      const isOnSecureDashboard = nextUrl.pathname.startsWith('/secure/dashboard');
-      if (isOnSecureDashboard) {
-        if (isLoggedIn) return true;
-        return false; // Redirect unauthenticated users to login page
-      } else if (isLoggedIn) {
-        return Response.redirect(new URL('/secure/dashboard', nextUrl));
-      }
-      return true;
+    async redirect({ url, baseUrl }) {
+      // After login, redirect users correctly
+      if (url.startsWith("/")) return `${baseUrl}${url}`;
+      if (url.startsWith(baseUrl)) return url;
+      return `${baseUrl}/secure/dashboard`;
     },
   },
 } satisfies NextAuthConfig;
