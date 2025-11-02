092ecc08ddeae274ad1f24e5f90c1cedb9ffc528
aliazahr <aali86362@outlook.com>
2025-11-02 10:00:09 -0500

    staged file restore

--- cryptocube/src/app/secure/coins/page.tsx
+++ cryptocube/src/app/secure/coins/page.tsx
@@ -1,5 +1,6 @@
 'use client'
 
+<<<<<<< Updated upstream
 import { Suspense } from 'react';
 import CoinsPageContent from './CoinsPageContent';
 
@@ -9,4 +10,24 @@ export default function CoinsPage() {
             <CoinsPageContent />
         </Suspense>
     );
+=======
+import { useState, useEffect } from "react"
+import { useSession } from 'next-auth/react';
+import { useSearchParams } from 'next/navigation';
+import Button from "@mui/material/Button"; // https://mui.com/material-ui/react-button/
+import SearchBar from '../components/SearchBar';
+import CoinsTable from '../components/CoinsTable';
+import AdvancedFiltersModal from '../components/AdvancedFiltersModal';
+import { applyAdvancedFilters, defaultFilters, type FiltersState } from '@/app/lib/filters';
+import { CoinData, CategoryData } from '@/app/lib/definitions';
+import { getCoinsList } from '../../lib/getCoinsList';
+import { getCategories } from '../../lib/getCategories';
+import { getFormatPrix, getFormatMarketCap, getFormatPercentage } from '@/app/lib/getFormatData';
+import { fetchWatchlistIds, addToWatchlist, removeFromWatchlist } from '@/app/lib/watchlistActions';
+import TrendingUpIcon from '@mui/icons-material/TrendingUp';
+import TrendingDownIcon from '@mui/icons-material/TrendingDown';
+
+export default function Page() {
+    
+>>>>>>> Stashed changes
 }
