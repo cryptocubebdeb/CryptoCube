6452f7983344c859aae1cabc7b7b73de87771d66
Nadine <2344893@bdeb.qc.ca>
2025-11-08 16:52:38 -0500

    Rajout du footer dans la nouvelle version

--- cryptocube/src/app/layout.tsx
+++ cryptocube/src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { SessionProvider } from "../app/components/SessionProvider";
+import Footer from "./components/Footer";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -28,7 +29,10 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <SessionProvider>{children}</SessionProvider>
+        <SessionProvider>
+          {children}
+          <Footer />
+        </SessionProvider>
       </body>
     </html>
   );
