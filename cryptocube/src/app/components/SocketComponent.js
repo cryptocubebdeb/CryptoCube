190a10e8ad2b6540309cc40bd74c03cbc43b1803
aliazahr <aali86362@outlook.com>
2025-11-16 15:12:28 -0500

    Test connection to the binance websocket. Page example withlive trade

--- cryptocube/src/app/components/SocketComponent.js
+++ cryptocube/src/app/components/SocketComponent.js
@@ -23,3 +23,5 @@ export default function SocketClient() {
 
   return <div>Socket Ready</div>;
 }
+
+
--- cryptocube/src/app/secure/socketTest/page.tsx
+++ cryptocube/src/app/secure/socketTest/page.tsx
@@ -1,5 +1,52 @@
 "use client";
 
+// Example of connecting to Binance WebSocket for BTCUSDT trades
+import { useEffect } from "react";
+
+export default function BinanceTest() {
+    useEffect(() => {
+        const ws = new WebSocket("wss://stream.binance.com/ws/btcusdt@trade");
+
+        ws.onopen = () => console.log("Binance WS connected");
+
+        ws.onmessage = (msg) => {
+            const data = JSON.parse(msg.data);
+            console.log("Trade:", data.p); // price
+        };
+
+        ws.onerror = (err) => console.log("WS error", err);
+        ws.onclose = () => console.log("Binance WS closed");
+
+        return () => ws.close();
+    }, []);
+
+    return <div>Page to run basic binance websocket test connection</div>;
+}
+
+/*
+    Binance WebSocket documentation:
+    https://developers.binance.com/docs/binance-spot-api-docs/web-socket-streams
+    View Section - Trade Streams
+
+    Example trade message:
+    {
+        "e": "trade",       // Event type
+        "E": 1672515782136, // Event time
+        "s": "BNBBTC",      // Symbol
+        "t": 12345,         // Trade ID
+        "p": "0.001",       // Price
+        "q": "100",         // Quantity
+        "T": 1672515782136, // Trade time
+        "m": true,          // Is the buyer the market maker?
+        "M": true           // Ignore
+    }
+*/
+
+
+//This is a connection example to our own websocket server
+
+/*"use client";
+
 import { useEffect } from "react";
 import { io } from "socket.io-client";
 
@@ -21,4 +68,4 @@ export default function SocketTestPage() {
   }, []);
 
   return <div>Socket test page</div>;
-}
+}*/
