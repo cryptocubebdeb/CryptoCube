4fbb1f039f2a796289b938502591feaa84a30d7b
Bao Ngoc Duong <2359970@bdeb.qc.ca>
2025-11-25 23:21:59 -0500

    margin pour specific coin

--- cryptocube/src/app/secure/specificCoin/[id]/page.tsx
+++ cryptocube/src/app/secure/specificCoin/[id]/page.tsx
@@ -96,13 +96,13 @@ export default async function Page({
         <div className={`min-h-screen w-full flex flex-col ${geologica.className}`}>
             <div className="flex flex-1 justify-center w-full">
                 {/* Main container */}
-                <div className="w-full max-w-[1600px] px-4 xl:px-8 flex items-start">
+                <div className="w-full max-w-[1600px] px-4 xl:px-8 flex items-start mt-15">
 
                     {/* Right column - details */}
-                    <div className="flex-[0.27] text-white p-8 text-xl rounded-[2px] shadow-md border-r border-white/20">
+                    <div className="flex-[0.27] text-white pr-10 text-xl rounded-[2px] shadow-md border-r border-white/20">
 
                         {/* Crypto Name and Logo */}
-                        <div className="flex items-center gap-4 p-4">
+                        <div className="flex items-center gap-4 p-4 pt-0">
                             {logo && (
                                 <Image
                                     src={logo}
