4ef2cd67f5c3491e04f0b2a46e3aa81429e7efbf
Bao Ngoc Duong <2359970@bdeb.qc.ca>
2025-12-12 03:49:02 -0500

    fix build issue, wrap in suspense for reset password page

--- cryptocube/src/app/auth/reset-password/page.tsx
+++ cryptocube/src/app/auth/reset-password/page.tsx
@@ -1,5 +1,12 @@
+"use client";
+
+import { Suspense } from "react";
 import ResetPasswordForm from "./ResetPasswordForm";
 
 export default function ResetPasswordPage() {
-  return <ResetPasswordForm />;
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <ResetPasswordForm />
+    </Suspense>
+  );
 }
